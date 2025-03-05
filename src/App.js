eadb3a3a62560fd83fe553f8c6a4ec09ce441d8a
Spiketrue <spiketrue@163.com>
2025-03-05 10:53:03 -0500

    implemented Router for navigation, created essential pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Resume from "./pages/Resume";
+import Portfolio from "./pages/Portfolio";
+import Photography from "./pages/Photography";
+import More from "./pages/More";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My personal website starts here
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/resume">Resume</Link></li>
+            <li><Link to="/portfolio">Porfolio</Link></li>
+            <li><Link to="/photography">Photography</Link></li>
+            <li><Link to="/more">More</Link></li>
+          </ul>
+        </nav>
+
+        {/* Page Routing */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/photography" element={<Photography />} />
+          <Route path="/more" element={<More />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/More.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function More() {
+  return <h1>More fun stuff</h1>;
+}
+
+export default More;
